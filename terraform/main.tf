96afae62d6fd20c192b1289877df2e5c9eb6ba72
Mohammed Sabry <Mohammed.Sabry@ibm.com>
2024-08-27 11:26:44 +0300

    k8s test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,63 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
-provider "aws" {
-  region = var.region
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30222
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
 }
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = var.clusterName
-}
-
-##
